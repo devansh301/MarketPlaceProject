89a1c10534eab7b6d4eff150202291ae85ddd16f
Bhargab Dhungel <bhargabdhungel@gmail.com>
2023-04-03 19:45:20 +0530

    3rd video

---
+++ backend/src/routes/admin/auth.js
@@ -0,0 +1,11 @@
+const express = require(`express`);
+const { signup, signin, requireSignIn } = require('../../controller/admin/auth.js')
+const router = express.Router();
+
+
+
+router.post('/admin/signup', signup);
+
+router.post('/admin/signin', signin);
+
+module.exports = router;
