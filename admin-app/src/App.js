2579ff041876ec82206f75cb8d3f4d17afaf46b9
Mohit Gupta <mohitgupta4357@gmail.com>
2023-04-04 02:18:19 +0530

    completed till video 4 yet a long way to go

--- admin-app/src/App.js
+++ admin-app/src/App.js
@@ -1,12 +1,25 @@
 import React from 'react'
+import {BrowserRouter as Router,Route,Routes} from 'react-router-dom'
 import './App.css';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from './container/Home'
+import Signin from './container/Signin';
+import Signup from './container/Signup';
+
+
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+    return (
+        <div className='App'>
+            <Router>
+                <Routes>
+                    <Route path="/" Component={Home}/>
+                    <Route path="/signin" Component={Signin}/>
+                    <Route path="/signup" Component={Signup}/>
+                </Routes>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
